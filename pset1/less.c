dac281a5a96979454b1057e62a4989861d5fe34f
bazzan <bazzanapalowo@gmail.com>
2022-01-02 08:10:57 +0100

    pset1: less mario
            Create a mario pyramid between 1 and 8

---
+++ pset1/less.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+int get_height(void);
+
+int main(void){
+
+    int length;
+    length = get_height();
+
+    while (length < 1 || length > 8){
+         length = get_height();
+    }
+
+    for (int i=1; i<length+1; i++){
+        // Create the space in front of #
+        for (int y=0; y<length-i; y++){
+            printf(" ");
+        }
+        // Populate the #
+        for (int j=0; j<i; j++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+int get_height(void){
+    int i;
+    // TODO: Take care of "letters or words" and "Enter key"
+    printf("Height: ");
+    scanf("%i", &i);
+    return i;
+}
