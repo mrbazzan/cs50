d95f4c89a0e2f753884df6b4dd52051f701e8d93
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2022-05-06 00:05:02 +0100

    descent parser: support for ()

--- rdParser/main.c
+++ rdParser/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 RECURSIVE DESCENT PARSER"
@@ -6,14 +7,23 @@ RECURSIVE DESCENT PARSER"
 
 typedef char * string;
 
+int parseSum(string *word);
+
 
 int parseFactor(string *word){
     if (**word >= '0' && **word <= '9'){
         return *(*word)++-'0';
     }
+    else if(**word == '('){
+        *(*word)++;
+        int exp = parseSum(word);
+        if (*(*word)++ == ')') return exp;
+    }
     else{
         printf("Only single digit are supported.\n");
     }
+    printf("Error!\n");
+    exit(24);
 }
 
 int parseProduct(string *word){
@@ -26,18 +36,18 @@ int parseProduct(string *word){
     return total;
 }
 
-int parseSum(string word){
-    int total = parseProduct(&word);
-    while(*word == '+'){
-        ++word;
-        int num = parseProduct(&word);
+int parseSum(string *word){
+    int total = parseProduct(word);
+    while(**word == '+'){
+        ++*word;
+        int num = parseProduct(word);
         total += num;
     }
     return total;
 }
 
 int main(void){
-    string x = "2*3+4*5";
-    printf("%d\n", parseSum(x));
+    string x = "2*(3+4)+2";
+    printf("%d\n", parseSum(&x));
     return 0;
 }
